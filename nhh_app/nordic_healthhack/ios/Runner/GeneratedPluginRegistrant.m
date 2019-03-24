c9a1d996cf753fe2176f0d439d1214dedbf77c1a
Javier Benitez <javier.benitezf51@gmail.com>
2019-03-24 10:13:00 +0000

    changed app name and app icon

--- nhh_app/nordic_healthhack/ios/Runner/GeneratedPluginRegistrant.m
+++ nhh_app/nordic_healthhack/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,10 +3,12 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <nhh_apis/NhhApisPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [NhhApisPlugin registerWithRegistrar:[registry registrarForPlugin:@"NhhApisPlugin"]];
 }
 
 @end
