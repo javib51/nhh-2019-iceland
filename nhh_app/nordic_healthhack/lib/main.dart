5612614d452351491345ee6f95df1247e9a68a1c
Javier Benitez <javier.benitezf51@gmail.com>
2019-03-24 12:38:30 +0000

    Loading screen

--- nhh_app/nordic_healthhack/lib/main.dart
+++ nhh_app/nordic_healthhack/lib/main.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:nordic_healthhack/screens/appointments_two_widget.dart';
 import 'package:nordic_healthhack/screens/appointments_widget.dart';
@@ -17,9 +16,11 @@ import 'package:nhh_apis/health_api/health_api.dart';
 import 'package:nhh_apis/arion/model/credit_cards.dart';
 import 'package:nhh_apis/arion/model/transactions.dart';
 import 'package:nhh_apis/digime/model/digime_activity_model.dart';
+
 //import 'package:nhh_apis/digime/model/digime_dailyactivity_model.dart';
 import 'package:nhh_apis/digime/model/digime_sleep_model.dart';
 import 'package:nordic_healthhack/data_algorithms.dart';
+import 'package:nordic_healthhack/widgets/initial_loader.dart';
 
 HealthApi healthApi_data;
 Transactions transactions_data;
@@ -29,39 +30,50 @@ Sleep sleep_data;
 void main() => runApp(App());
 
 class App extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-
-    getAllHealth().then((res) {
+  Future<bool> initModules() async {
+    await getAllHealth().then((res) {
       print(res);
       healthApi_data = res;
     });
 
     ArionAPI arion = ArionAPI();
-    arion.getCards().then((res) {
+    await arion.getCards().then((res) {
       print(res.toString());
       arion.getTransactions(res.creditCard[0].cardId).then((res) {
         print(res.transaction.length.toString());
-        transactions_data =res;
+        transactions_data = res;
       });
     });
 
-    loadDailyActivity().then((res){
+    await loadDailyActivity().then((res) {
       print(res.fileData[0].caloriesbmr);
     });
-    loadActivity().then((res){
+    await loadActivity().then((res) {
       print(res.fileData[0].calories);
-      activity_data=res;
+      activity_data = res;
     });
-    loadSleep().then((res){
-      print(res.fileData.length.toString()+"d");
-      sleep_data=res;
+    await loadSleep().then((res) {
+      print(res.fileData.length.toString() + "d");
+      sleep_data = res;
     });
+    return true;
+  }
 
-
+  @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      home: TestsWidget(),
+      title: 'ChitChat',
+      home: FutureBuilder(
+          future: initModules(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.hasData && snapshot.data != null) {
+              return (!snapshot.data) ?
+              Scaffold(body: Center(child:ColorLoader3())) : TestsWidget();
+            } else {
+              return Scaffold(body: Center(child:ColorLoader3()));
+            }
+          }),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
