4a2ba93adcccaf505ed8cb28eb0736409837c24f
waliyah <waliyah02@gmail.com>
2019-03-24 08:24:03 +0000

    Update medical_profile_widget.dart

--- nhh_app/nordic_healthhack/lib/screens/medical_profile_widget.dart
+++ nhh_app/nordic_healthhack/lib/screens/medical_profile_widget.dart
@@ -4,169 +4,80 @@ import 'package:nordic_healthhack/widgets/app_bar.dart';
 
 
 class MedicalProfileWidget extends StatelessWidget {
-  
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        constraints: BoxConstraints.expand(),
-        decoration: BoxDecoration(
-          color: Color.fromARGB(255, 255, 255, 255),
-        ),
-        child: Stack(
-          fit: StackFit.expand,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Stack(
-                    children: [
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Container(
-                            constraints: BoxConstraints.expand(width: 319),
-                            margin: EdgeInsets.only(left: 20, top: 33),
-                            decoration: BoxDecoration(
-                              color: Color.fromARGB(233, 135, 221, 167),
-                              boxShadow: [
-                                BoxShadow(
-                                  color: Color.fromARGB(128, 0, 0, 0),
-                                  offset: Offset(0, 2),
-                                  blurRadius: 4,
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Container(
-                            margin: EdgeInsets.only(left: 37, top: 47),
-                            child: Text(
-                              "John Doe",
-                              style: TextStyle(
-                                color: Color.fromARGB(255, 75, 74, 75),
-                                fontSize: 16,
-                                fontFamily: "Open Sans",
-                                fontWeight: FontWeight.w700,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Expanded(
-                            flex: 1,
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              crossAxisAlignment: CrossAxisAlignment.stretch,
-                              children: [
-                                Container(
-                                  constraints: BoxConstraints.expand(width: 85),
-                                  margin: EdgeInsets.only(top: 44, right: 49),
-                                  decoration: BoxDecoration(
-                                    color: Color.fromARGB(255, 172, 172, 172),
-                                    borderRadius: BorderRadius.all(Radius.circular(42.5)),
-                                    image: new DecorationImage(
-                                      image: ExactAssetImage("assets/images/man.png"),
-                                      fit: BoxFit.none,
-                                    ),
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: Container(
-                    margin: EdgeInsets.only(left: 37, top: 4),
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Container(
+
+            decoration: BoxDecoration(
+
+              image: DecorationImage(
+                alignment: Alignment.topCenter,
+                image: AssetImage('assets/images/man.png'),
+                fit: BoxFit.scaleDown,
+              ),
+            ),
+          ),
+          Positioned(
+            top: 150,
+            left: 10.0,
+            right: 10.0,
+            child: Card(
+              elevation: 8.0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8.0),
+              ),
+              child: Column(
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      "32",
+                      "John Doe",
                       style: TextStyle(
-                        color: Color.fromARGB(255, 75, 74, 75),
-                        fontSize: 16,
-                        fontFamily: "Open Sans",
-                        fontWeight: FontWeight.w300,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
                       ),
-                      textAlign: TextAlign.center,
                     ),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.only(left: 37, top: 4),
-                        child: Text(
-                          "177 cm , 70kg",
-                          style: TextStyle(
-                            color: Color.fromARGB(255, 75, 74, 75),
-                            fontSize: 16,
-                            fontFamily: "Open Sans",
-                            fontWeight: FontWeight.w300,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Container(),
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Age: 32"),
                   ),
-                ),
-                Expanded(
-                  flex: 1,
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Container(
-                        constraints: BoxConstraints.expand(width: 138),
-                        margin: EdgeInsets.only(left: 20),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 216, 216, 216),
-                          border: Border.all(
-                            color: Color.fromARGB(255, 151, 151, 151),
-                            width: 1,
-                          ),
-                        ),
-                      ),
-                      Expanded(
-                        flex: 1,
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.end,
-                          crossAxisAlignment: CrossAxisAlignment.stretch,
-                          children: [
-                            Container(
-                              constraints: BoxConstraints.expand(width: 138),
-                              margin: EdgeInsets.only(right: 21),
-                              decoration: BoxDecoration(
-                                color: Color.fromARGB(255, 216, 216, 216),
-                                border: Border.all(
-                                  color: Color.fromARGB(255, 151, 151, 151),
-                                  width: 1,
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Height: 170cm"),
                   ),
-                ),
-              ],
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Weight: 70kg"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Sex: Male"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Bloodgroup: A+"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                        "Insurance: All-Inclusive"),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
+      ),
     );
   }
 }
